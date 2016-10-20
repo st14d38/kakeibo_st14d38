e5797f90fa49aec517be6234baf57a55e02300ad
Keitaro MATSUMOTO <st14d38@gm.ibaraki-ct.ac.jp>
2016-10-20 16:53:30 +0900

    統合成功

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
+#include "sum.h"
+#include "average.h"
+
+#define number 5
 
 int main()
 {
-	int payments[5]
-	int i
-	for(i=0; i<5; i++) {
+	int payments[number];
+	int i;
+	for(i=0; i<number; i++) {
 		scanf("%d", &payments[i]);
 	}
 
+	printf("合計値:%d\n", sum(payments));
+	printf("平均値:%d\n", average(payments));
 	return 0;
 }
